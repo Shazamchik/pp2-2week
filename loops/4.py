ed990f5dbc37254fc33593d22ddf9516f0ee1203
BeibarysShayakhmetov <shazam.chik88@mail.ru>
2026-01-25 10:58:30 +0500

    add loop examples

---
+++ loops/4.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
